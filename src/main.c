b8786ccc0f4fc3af8888ec37da1a56941e651dbc
Szpila123 <bartoszszpila98@gmail.com>
2023-01-13 20:05:49 +0100

    Szpila123/master: Environment ready

--- src/main.c
+++ src/main.c
@@ -3,13 +3,15 @@
 
 const uint LED_PIN = 25;
 
-int main() {
-    bi_decl(bi_program_description("First Blink"));
-    bi_decl(bi_1pin_with_name(LED_PIN, "On-board LED"));
+int main()
+{
+    bi_decl(bi_program_description("OV7670 camera module"));
+    bi_decl(bi_1pin_with_name(LED_PIN, "LED pin"));
 
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
-    while (true) {
+    while (true)
+    {
         gpio_put(LED_PIN, 0);
         sleep_ms(250);
         gpio_put(LED_PIN, 1);
